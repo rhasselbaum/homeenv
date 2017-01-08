a30df8524cb52ce203fc21214ac0236685a18b37
Rob Hasselbaum <rob@hasselbaum.net>
2017-01-08 12:39:55 -0500

    Add alias for SSH to waypoint

--- .config/fish/conf.d/abbrs.fish
+++ .config/fish/conf.d/abbrs.fish
@@ -5,5 +5,6 @@ if not set -q fish_abbrs_initialized
   abbr -a caprica ssh rob@caprica.hasselbaum.net
   abbr -a gemenon ssh rob@gemenon.hasselbaum.net
   abbr -a periwinkle ssh rob@periwinkle.hasselbaum.net
+  abbr -a waypoint ssh rob@waypoint.hasselbaum.net
   set -U fish_abbrs_initialized
 end
