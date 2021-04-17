9e8a0322f95646683613ab8e464d96d7967366f5
Rob Hasselbaum <rob@hasselbaum.net>
2021-04-17 12:06:29 -0400

    Update SSH shortcuts

--- .config/fish/conf.d/abbrs.fish
+++ .config/fish/conf.d/abbrs.fish
@@ -5,7 +5,8 @@ if not set -q fish_abbrs_initialized
   abbr -a caprica ssh rob@caprica.hasselbaum.net
   abbr -a gemenon ssh rob@gemenon.hasselbaum.net
   abbr -a periwinkle ssh rob@periwinkle.hasselbaum.net
-  abbr -a waypoint ssh rob@waypoint.hasselbaum.net
+  abbr -a iris ssh rob@iris.hasselbaum.net
+  addr -a icecast ssh -i ~/.ssh/icecast.pem admin@icecast.hasselbaum.net
   abbr -a llt 'ls -aFlt | head'
   set -U fish_abbrs_initialized
 end
