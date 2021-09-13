b5fd18d5d291b6f46ce43b6bd66b88627931b6b2
Rob Hasselbaum <robhas@amazon.com>
2021-09-13 13:39:45 -0400

    Update syntax to add ~/bin to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,3 @@
 # Add BrazilCLI to PATH if its location is known.
 test -n "$BRAZIL_CLI_BIN"; and set PATH "$BRAZIL_CLI_BIN" $PATH
-fish_add_path ~/bin
+set -U fish_user_paths ~/bin $fish_user_paths
