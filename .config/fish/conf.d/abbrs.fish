0959e3ef7b46f025a8100095aaa566244d762b6a
Rob Hasselbaum <rob@hasselbaum.net>
2024-09-29 09:36:35 -0400

    Update host abbreviations

--- .config/fish/conf.d/abbrs.fish
+++ .config/fish/conf.d/abbrs.fish
@@ -3,8 +3,7 @@ if status --is-interactive
   abbr -a scorpia ssh rob@scorpia.hasselbaum.net
   abbr -a caprica ssh rob@caprica.hasselbaum.net
   abbr -a gemenon ssh rob@gemenon.hasselbaum.net
-  abbr -a periwinkle ssh rob@periwinkle.hasselbaum.net
   abbr -a iris ssh rob@iris.hasselbaum.net
-  abbr -a icecast ssh -i ~/.ssh/icecast.pem admin@icecast.hasselbaum.net
+  abbr -a aquaria ssh rob@aquaria.hasselbaum.net
   abbr -a llt 'ls -aFlt | head'
 end
