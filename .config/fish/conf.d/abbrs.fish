8d0b36eaec51dd3706825f40a68b4739d7aa087c
Rob Hasselbaum <rob@hasselbaum.net>
2025-04-19 13:37:43 -0400

    Replace Iris with Amethyst

--- .config/fish/conf.d/abbrs.fish
+++ .config/fish/conf.d/abbrs.fish
@@ -3,7 +3,7 @@ if status --is-interactive
   abbr -a scorpia ssh rob@scorpia.hasselbaum.net
   abbr -a caprica ssh rob@caprica.hasselbaum.net
   abbr -a gemenon ssh rob@gemenon.hasselbaum.net
-  abbr -a iris ssh rob@iris.hasselbaum.net
+  abbr -a amethyst ssh rob@amethyst.hasselbaum.net
   abbr -a aquaria ssh rob@aquaria.hasselbaum.net
   abbr -a llt 'ls -aFlt | head'
 end
